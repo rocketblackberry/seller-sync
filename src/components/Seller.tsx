cabe1d8073f1dbf28ee3d1bf88dc3d8ccd8a9824
rocketblackberry <rocketblackberry@gmail.com>
2025-02-06 22:20:27 +0900

    フォールバックを設定

--- src/components/Seller.tsx
+++ src/components/Seller.tsx
@@ -12,8 +12,8 @@ const sellers = [
 export default function Seller() {
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     if (e.target.value === "0") {
-      const EBAY_CLIENT_ID = process.env.NEXT_PUBLIC_EBAY_CLIENT_ID;
-      const EBAY_REDIRECT_URL = process.env.NEXT_PUBLIC_EBAY_REDIRECT_URL;
+      const EBAY_CLIENT_ID = process.env.NEXT_PUBLIC_EBAY_CLIENT_ID || "";
+      const EBAY_REDIRECT_URL = process.env.NEXT_PUBLIC_EBAY_REDIRECT_URL || "";
       const ebayLoginUrl = `https://auth.ebay.com/oauth2/authorize?client_id=${EBAY_CLIENT_ID}&redirect_uri=${encodeURIComponent(EBAY_REDIRECT_URL)}&response_type=code&scope=https://api.ebay.com/oauth/api_scope/sell.inventory`;
       window.open(ebayLoginUrl, "_blank");
     }
